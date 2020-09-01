d24428fd04359310d3a79a5d67a966812009d1ed
Jocelyn <joeymmiller@gmail.com>
2020-09-01 09:51:53 -0400

    Initial Commit

---
+++ AboutMeApp2.0/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AboutMeApp2.0
+//
+//  Created by Jocelyn Miller on 9/1/20.
+//  Copyright © 2020 Jocelyn Miller. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AboutMeApp2.0/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  AboutMeApp2.0
+//
+//  Created by Jocelyn Miller on 9/1/20.
+//  Copyright © 2020 Jocelyn Miller. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
